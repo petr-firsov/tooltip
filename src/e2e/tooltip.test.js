f6abca7470f204e5b8eaa020c6efbc90a1388a3b
petr-firsov <125796227+petr-firsov@users.noreply.github.com>
2024-10-30 23:40:19 +0300

    Отключил тесты 6

--- src/e2e/tooltip.test.js
+++
@@ -1,9 +0,0 @@
-// import puppeteer from 'puppeteer';
-
-// test('should show tooltip', async () => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     await page.goto('file://C:/Users/peter/Desktop/tooltip/dist/index.html');
-//     await page.locator('btn').click;
-// });
-
