e2ddeaaa230c9856d50667c3d138bf01922c842a
petr-firsov <125796227+petr-firsov@users.noreply.github.com>
2024-11-20 17:19:20 +0300

    Должно получиться

--- .puppeteer.js
+++ .puppeteer.js
@@ -1,9 +1,8 @@
-/**
- * @type {import('puppeteer').Configuration}
- */
+const {join} = require('path');
 
+/**
+ * @type {import("puppeteer").Configuration}
+ */
 module.exports = {
-    chrome: {
-        skipDownload: false,
-    }
-}
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
